0b654761dfce00ceb0f9de583ef37c10d9673410
franco <x.francoruiz@gmail.com>
2022-01-12 03:03:23 -0300

    create file

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const fs = require("fs");
+
 console.clear();
 console.log("--------------------");
 console.log("Calculator");
 console.log("--------------------");
 
+// vars
 let base = 5;
 let output = "";
 
+// calculate multipliers
 for (let index = 1; index <= 10; index++) {
   output += `${base} x ${index} = ${base * index}\n`;
 }
 
-console.log(output);
+// create and write file
+fs.writeFile(`result-base-${base}.txt`, output, (error) => {
+  if (error) throw error;
+  console.log("Success!!");
+});
